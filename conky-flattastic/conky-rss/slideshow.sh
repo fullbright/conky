b836fa6c359658e1154a6e973516b813c5763661
Full Bright <full3right@gmail.com>
2016-02-21 17:16:13 +0100

    Make rings foreground gray

--- conky-flattastic/conky-rss/slideshow.sh
+++ conky-flattastic/conky-rss/slideshow.sh
@@ -8,11 +8,11 @@ N=${#files[@]}
 file=${files[$TIME%$N]} 
 echo '${image '$file' -p 3,3 -s 315x190}'
 for I in {1..6}
-   do
+    do
       file1=${files[$TIME%$N+$I]}
-      echo '${image '$file1' -p '$R',199 -s 50x31}'
+      echo '${image '$file1' -p '$R',199 -s 50x31}' >> /dev/null
       R=`expr $R + 53`
-   done
+    done
 
 
 
