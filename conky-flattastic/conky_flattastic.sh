1a634d18862dac746445ac0ea8cdb8efb692b6b3
Full Bright <full3right@gmail.com>
2016-07-02 10:23:39 +0200

    Correct launch script

--- conky-flattastic/conky_flattastic.sh
+++ conky-flattastic/conky_flattastic.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
-sleep 20
-conky -c ~/.conky/conky-flattastic/conky-ring/conkyrc_ring &
-conky -c ~/.conky/conky-flattastic/conky-weather/conkyrc_weather &
-conky -c ~/.conky/conky-flattastic/conky-calendar/conkyrc_calendar &
-conky -c ~/.conky/conky-flattastic/conky-rss/conkyrc_rss_ubuntu &
-conky -c ~/.conky/conky-flattastic/conky-dash/conkyrc_dash_ubuntu
+
+LOGFILE=~/.conky/conky-flattastic/conky_flattastic.log
+
+echo "Executing script as `whoami`" >> $LOGFILE 2>&1
+echo "Sleeping 2 secs before starting the widgets" >> $LOGFILE 2>&1
+sleep 2
+
+echo "Starting widget conky ring" >> $LOGFILE 2>&1
+conky -c ~/.conky/conky-flattastic/conky-ring/conkyrc_ring & >> $LOGFILE 2>&1
+
+sleep 2
+echo "Starting widget conky weather" >> $LOGFILE 2>&1
+conky -c ~/.conky/conky-flattastic/conky-weather/conkyrc_weather & >> $LOGFILE 2>&1
+
+sleep 2
+echo "Starting widget conky calendar" >> $LOGFILE 2>&1
+conky -c ~/.conky/conky-flattastic/conky-calendar/conkyrc_google_calendar & >> $LOGFILE 2>&1
+
+sleep 2
+echo "Starting widget conky rss" >> $LOGFILE 2>&1
+conky -c ~/.conky/conky-flattastic/conky-rss/conkyrc_rss_ubuntu & >> $LOGFILE 2>&1
+
+sleep 2
+echo "Starting widget conky dash" >> $LOGFILE 2>&1
+conky -c ~/.conky/conky-flattastic/conky-dash/conkyrc_dash_ubuntu >> $LOGFILE 2>&1
+
 exit 0
